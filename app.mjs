42b2a45e9d9fcf2d0cba82bf05d0170c1ca00725
Alex Rosso <alexrosso1025@gmail.com>
2024-11-14 23:21:32 -0500

    3 minutes await for each execution and new report for performance variables

--- app.mjs
+++ app.mjs
@@ -23,11 +23,16 @@ async function setupBrowser(url) {
 }
 
 // Función para ejecutar Lighthouse y obtener los datos necesarios
-async function getLighthouseData(url) {
+async function getLighthouseData(url, metricsType) {
+  const audits = metricsType === "core"
+    ? ["performance", "accessibility", "best-practices", "seo"]
+    : ["first-contentful-paint", "largest-contentful-paint", "speed-index", "total-blocking-time", "cumulative-layout-shift"];
+
   const { lhr } = await lighthouse(url, {
     port: 9222,
     output: ["json", "html"],
     onlyCategories: ["performance", "accessibility", "best-practices", "seo", "pwa"],
+    onlyAudits: audits,
   });
   
   return {
@@ -36,6 +41,11 @@ async function getLighthouseData(url) {
     accessibility: lhr.categories.accessibility.score * 100,
     bestPractices: lhr.categories["best-practices"].score * 100,
     seo: lhr.categories.seo.score * 100,
+    firstContentfulPaint: parseTime(lhr.audits["first-contentful-paint"].displayValue),
+    largestContentfulPaint: parseTime(lhr.audits["largest-contentful-paint"].displayValue),
+    speedIndex: parseTime(lhr.audits["speed-index"].displayValue),
+    totalBlockingTime: parseTime(lhr.audits["total-blocking-time"].displayValue),
+    cumulativeLayoutShift: parseFloat(lhr.audits["cumulative-layout-shift"].displayValue),
   };
 }
 
@@ -58,11 +68,16 @@ async function saveDataToExcel(data, component) {
     { header: "Accessibility", key: "accessibility", width: 20 },
     { header: "Best Practices", key: "bestPractices", width: 20 },
     { header: "SEO", key: "seo", width: 20 },
+    { header: "First Contentful Paint", key: "firstContentfulPaint", width: 20},
+    { header: "Largest Contentful Paint", key: "largestContentfulPaint", width: 20},
+    { header: "Speed Index", key: "speedIndex", width: 20},
+    { header: "Total Blocking Time", key: "totalBlockingTime", width: 20},
+    { header: "Cumulative Layout Shift", key: "cumulativeLayoutShift", width: 20},
   ];
 
   worksheet.addRow(data);
   await workbook.xlsx.writeFile(fileName);
-  console.log("Datos añadidos al archivo Excel:", fileName);
+  console.log("Data added to the Excel file:", fileName);
 }
 
 // Función para leer datos desde Excel
@@ -83,18 +98,23 @@ async function readDataFromExcel(component) {
           accessibility: row.getCell("C").value,
           bestPractices: row.getCell("D").value,
           seo: row.getCell("E").value,
+          firstContentfulPaint: row.getCell("F").value,
+          largestContentfulPaint: row.getCell("G").value,
+          speedIndex: row.getCell("H").value,
+          totalBlockingTime: row.getCell("I").value,
+          cumulativeLayoutShift: row.getCell("J").value,
         });
       }
     });
   } else {
-    console.log("El archivo no existe.");
+    console.log("The file doesn't exist.");
   }
 
   return data;
 }
 
 // Función para generar el gráfico en base a los datos
-async function generateChartImage(data, component) {
+async function generateChartImage(data, component,metricsType) {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
@@ -102,36 +122,51 @@ async function generateChartImage(data, component) {
   await page.addScriptTag({ url: "https://cdn.jsdelivr.net/npm/chart.js" });
   await page.waitForFunction(() => typeof Chart !== "undefined");
 
-  const averages = {
-    performance: (data.reduce((sum, item)=> sum + item.performance, 0) / data.length).toFixed(2),
-    accessibility: (data.reduce((sum, item)=> sum + item.accessibility, 0) / data.length).toFixed(2),
-    bestPractices: (data.reduce((sum, item)=> sum + item.bestPractices, 0) / data.length).toFixed(2),
-    seo: (data.reduce((sum, item)=> sum + item.seo, 0) / data.length).toFixed(2),
+  let metricsConfig = {};
+  if(metricsType === "general"){
+    metricsConfig = {
+      fields: ["performance","accessibility","bestPractices","seo"],
+      colors: ["rgb(75, 192, 192)", "rgb(0, 51, 51)", "rgb(153, 204, 0)", "rgb(51, 0, 0)"],
+      averagesText: (averages) =>
+        `Averages: Performance: ${averages.performance} | Accessibility: ${averages.accessibility} | Best Practices: ${averages.bestPractices} | SEO: ${averages.seo}`,
+    };
+  } else if(metricsType === "performance"){
+    metricsConfig = {
+      fields: ["firstContentfulPaint", "largestContentfulPaint", "speedIndex", "totalBlockingTime", "cumulativeLayoutShift"],
+      colors: ["rgb(255, 128, 0)", "rgb(51, 51, 255)", "rgb(255, 102, 255)", "rgb(0, 0, 0)", "rgb(51, 255, 255)"],
+      averagesText: (averages) =>
+        `Averages: FCP: ${averages.firstContentfulPaint} | LCP: ${averages.largestContentfulPaint} | Speed Index: ${averages.speedIndex} | TBT: ${averages.totalBlockingTime} | CLS: ${averages.cumulativeLayoutShift}`,
+    };
   }
+
+  const averages = metricsConfig.fields.reduce((acc, field) => {
+    acc[field] = (data.reduce((sum, item) => sum + item[field], 0) / data.length).toFixed(2);
+    return acc;
+  }, {});
 
   const chartConfig = {
     type: "line",
     data: {
       labels: data.map((item) => item.timestamp),
-      datasets: [
-        { label: "Performance", data: data.map((item) => item.performance), borderColor: "rgb(75, 192, 192)", fill: false },
-        { label: "Accessibility", data: data.map((item) => item.accessibility), borderColor: "rgb(0, 51, 51)", fill: false },
-        { label: "Best Practices", data: data.map((item) => item.bestPractices), borderColor: "rgb(153, 204, 0)", fill: false },
-        { label: "SEO", data: data.map((item) => item.seo), borderColor: "rgb(51, 0, 0)", fill: false },
-      ],
+      datasets: metricsConfig.fields.map((field, index) => ({
+        label: field.charAt(0).toUpperCase() + field.slice(1),
+        data: data.map((item) => item[field]),
+        borderColor: metricsConfig.colors[index],
+        fill: false,
+      })),
     },
     options: {
       plugins: {
-        subtitle:{
+        subtitle: {
           display: true,
-          text: `Avg Perfomance: ${averages.performance} | Avg Accessibility: ${averages.accessibility} | Avg Best Practices: ${averages.bestPractices} | Avg Seo: ${averages.seo}`
+          text: metricsConfig.averagesText(averages),
         },
         legend: {
           display: true,
-          position: 'right',
-        }
-      }
-    }
+          position: "right",
+        },
+      },
+    },
   };
 
   await page.evaluate((chartConfig) => {
@@ -142,8 +177,19 @@ async function generateChartImage(data, component) {
     new Chart(canvas.getContext("2d"), chartConfig);
   }, chartConfig);
 
-  await page.screenshot({ path: `lighthouse-report-${component}.png`, fullPage: true });
+  // Guardar el gráfico en un archivo de imagen
+  await page.screenshot({ path: `lighthouse-report-${component}-${metricsType}.png`, fullPage: true });
   await browser.close();
+}
+
+function parseTime(value) {
+  if (value.endsWith('ms')) {
+    return parseFloat(value.replace('ms', '').replace(',', '.')); 
+  } else if (value.endsWith('s')) {
+    return parseFloat(value.replace('s', '')) * 1000;
+  } else {
+    return parseFloat(value); // Default case
+  }
 }
 
 // Función principal para ejecutar el flujo completo
@@ -151,12 +197,18 @@ async function runLighthouseWorkflow(url) {
   const component = process.env.COMPONENT;
   const { browser } = await setupBrowser(url);
 
-  const lighthouseData = await getLighthouseData(url);
-  await saveDataToExcel(lighthouseData, component);
+  for (let i = 0; i < 5; i++) {
+    const lighthouseData = await getLighthouseData(url);
+    await saveDataToExcel(lighthouseData, component);
+    
+    console.log(`Awaiting 3 minutes before the next execution: ${i+1} of 5`);
+    await new Promise((resolve) => setTimeout(resolve, 3 * 60 * 1000));
+  }
 
   const excelData = await readDataFromExcel(component);
-  await generateChartImage(excelData, component);
-
+  
+  await generateChartImage(excelData, component, "general");
+  await generateChartImage(excelData, component, "performance");
   await browser.close();
 }
 
